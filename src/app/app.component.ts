b727b89042f62c0971f8a9bd5d77bce9f82acf38
Volodymir Tymtsias <vovanuch2007@gmail.com>
2024-02-26 16:42:20 +0200

    Added basic Angular template features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+const todos = [
+  {id: 1, title: 'HTML+CSS', completed: true},
+  {id: 2, title: 'JS', completed: false},
+  {id: 3, title: 'React', completed: false},
+  {id: 4, title: 'Vue.js', completed: false},
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-todo-app';
+  editing = false;
+  todos = todos;
 }
